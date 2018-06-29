e0ec1ac438b954565f13b7a3815dbd85e2dfa34b
martinenti <40662211+martinenti@users.noreply.github.com>
2018-06-29 10:14:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,15 @@ if (message.content === '+help') {
  message.author.sendEmbed(embed);
    }
 });
- 
+ client.on('message', message => {
+     if (message.content === (prefix + "help")) {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#8650a7")
+  .addField("Done" , " تــــم ارســالك في الخــاص")
+  message.channel.sendEmbed(embed);
+    }
+});
 
 
 
