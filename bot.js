6bf91cc41da7e7d6823ac7b2be29ebcff10541a5
martinenti <40662211+martinenti@users.noreply.github.com>
2018-06-29 10:38:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const prefix = '+'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`Nothing`,"http://twitch.tv/S-F
-
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
