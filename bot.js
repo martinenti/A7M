9b8374e3952407079968cdaa03c71697367fe5e2
martinenti <40662211+martinenti@users.noreply.github.com>
2018-06-29 10:37:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '+'
-
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F
 
   console.log('')
   console.log('')
