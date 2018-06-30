9930327de2de1d434418fb21dedf6e9d1cf3a609
martinenti <40662211+martinenti@users.noreply.github.com>
2018-06-30 02:58:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('ready', () => {
-     client.user.setActivity(" +help",{type: 'WATCHING'})
+const prefix = '+'
 
-});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -153,6 +154,16 @@ client.on("message", message => {
 });
 
 
+client.on('message', message => {
+    if (message.content === "+roles") {
+		if(!message.channel.guild) return;
+        var roles = message.guild.roles.map(roles => `${roles.name}, `).join(' ')
+        const embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .addField('Roles:',`**[${roles}]**`)
+        message.channel.sendEmbed(embed);
+    }
+});
 
 
 
@@ -160,8 +171,60 @@ client.on("message", message => {
 
 
 
+client.on('message', message => {
+    const prefix = '+'
+var args = message.content.split(" ").slice(1);    
+if(message.content.startsWith(prefix + 'id')) {
+var year = message.author.createdAt.getFullYear()
+var month = message.author.createdAt.getMonth()
+var day = message.author.createdAt.getDate()
+var men = message.mentions.users.first();  
+let args = message.content.split(' ').slice(1).join(' ');
+if (args == '') {
+var z = message.author;
+}else {
+var z = message.mentions.users.first();
+}
 
+let d = z.createdAt;          
+let n = d.toLocaleString();   
+let x;                       
+let y;                        
 
+if (z.presence.game !== null) {
+y = `${z.presence.game.name}`;
+} else {
+y = "No Playing... |??.";
+}
+let embed = new Discord.RichEmbed()
+.setColor("#502faf")
+.addField(': ?? | اسمك',`**<@` + `${z.id}` + `>**`, true)
+.addField(': ?? | ايديك', "**"+ `${z.id}` +"**",true)
+.addField(': ? | Playing','**'+y+'**' , true)
+.addField(': ?? | تاق حق حسابك',"**#" +  `${z.discriminator}**`,true)
+.addField('**: ?? | التاريح الذي انشئ فيه حسابك**', message.author.createdAt.toLocaleString())
+.addField("**: ? | تاريخ دخولك للسيرفر**", message.member.joinedAt.toLocaleString())    
+
+.setThumbnail(`${z.avatarURL}`)
+.setFooter(message.author.username, message.author.avatarURL)
+
+message.channel.send({embed});
+    if (!message) return message.reply('**ضع المينشان بشكل صحيح  ? **').catch(console.error);
+
+}
+
+});
+
+client.on("message", message => {
+ if (message.content === "-support") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('© Heroes Bot:heart: جميع الحقو محفوظة 2017 لــبوت')  
+      .addField('سيرفر الدعم الفني', `  https://discord.gg/6Q6Vn5N`)
+  message.author.send({embed});
+
+ }
+});
 
 
 
