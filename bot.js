84ea478c9d5ebc9b76918d65094fdf71e6db5747
martinenti <40662211+martinenti@users.noreply.github.com>
2018-06-30 03:15:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -287,19 +287,7 @@ client.on('message', message => {
     }
  });
 
- client.on('message', message => {
-   if (message.content.startsWith("عقاب")) {
-                if(!message.channel.guild) return message.reply('** This command only for servers**');
-  var embed = new Discord.RichEmbed()
-  .setColor('RANDOM')
-   .setThumbnail(message.author.avatarURL) 
- .addField('WOlF BOT' ,
-  `${Za7f[Math.floor(Math.random() * Za7f.length)]}`)
-  message.channel.sendEmbed(embed);
-  console.log('[38ab] Send By: ' + message.author.username)
-    }
-});
-
+ 
 client.on('message', function(message) {
     if(message.content.startsWith(prefix + 'roll')) {
         let args = message.content.split(" ").slice(1);
