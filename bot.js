4ee975d66ed52e079a05e24078bba16f5cba83a0
martinenti <40662211+martinenti@users.noreply.github.com>
2018-06-30 04:11:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -587,20 +587,6 @@ client.on('message', message = {
 
 
 
-client.on("message", message => {
-     var prefix = "+";
-    if(message.content.startsWith(prefix + 'روم مؤقت')) {
-     let args = message.content.split(" ").slice(1);
-       var nam = args.join(' ');
-    
-      if(!message.member.hasPermission('ADMINISTRATOR')) return    message.channel.send('`ADMINISTRATOR` للأسف هذه الخاصية تحتاج الى ').then(msg => msg.delete(6000))
-      if (!nam) return message.channel.send(`<@${message.author.id}> يجب عليك ادخال اسم`).then(msg => msg.delete(10000))
-      message.guild.createChannel(nam, 'voice').then(c => setTimeout(() => c.delete(), 120000)) // كل 60 تساوي دقيقة عدل عليها الوقت لي تبيه 
-      message.channel.send(`? TemporarySound : \`${nam}\``).then(c => setTimeout(() => c.edit(`<@${message.author.id}> ?  انتهى وقت الروم الصوتي`), 120000))  // 120000 دقيقتان
-    }
-    });
-	 
-	 
 	 
 	 
 	  const zalgo = require('zalgolize');
