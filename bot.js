6c540f94a3eb08d112fc8e939e44164071ac0805
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-02 00:25:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -647,7 +647,7 @@ if (message.content.startsWith(prefix + 'نقاطي')) {
   
 });
 
-const fs = require('fs');
+
 let points = JSON.parse(fs.readFileSync('./Points.json'));
 client.on('message', message => {
     if (!points[message.author.id]) points[message.author.id] = { 
