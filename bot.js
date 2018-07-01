34f46b7c6e89636f87a329abed6b7bf7db4dfd04
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-01 15:04:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -597,42 +597,9 @@ client.on("message", message => {
 
 
 
-client.on('message', message => {
-  if (message.author.kick) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "kick") {
-               if(!message.channel.guild) return;
-         
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("You Don't Have KICK_MEMBERS Permission").then(msg => msg.delete(5000));
-  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("I Don't Have KICK_Members Permission");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-
-  if (message.mentions.users.size < 1) return message.reply("منشن شخص");
-  if(!reason) return message.reply ("اكتب سبب الطرد");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("لايمكنني طرد شخص اعلى من رتبتي");
-
-  message.guild.member(user).kick(7, user);
-
-  const banembed = new Discord.RichEmbed()
-  .setAuthor(Kicked !, user.displayAvatarURL)
-  .setColor("RANDOM")
-  .setTimestamp()
-  .addField("User:",  '[ ' + ${user.tag} + ' ]')
-  .addField("By:", '[ ' + ${message.author.tag} + ' ]')
-  .addField("Reason:", '[ ' + ${reason} + ' ]')
-  client.channels.get("log").send({embed : banembed})
-}
-});
 
 
+var prefix = "+"
 client.on('message', message => {
   if (message.author.ban) return;
   if (!message.content.startsWith(prefix)) return;
@@ -673,6 +640,41 @@ client.on('message', message => {
 
 
 
+var prefix = "+"
+client.on('message', message => {
+  if (message.author.kick) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+               if(!message.channel.guild) return;
+
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("You Don't Have KICK_MEMBERS Permission").then(msg => msg.delete(5000));
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("I Don't Have KICK_Members Permission");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+
+  if (message.mentions.users.size < 1) return message.reply("منشن شخص");
+  if(!reason) return message.reply ("اكتب سبب الطرد");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("لايمكنني طرد شخص اعلى من رتبتي");
+
+  message.guild.member(user).kick(7, user);
+
+  const banembed = new Discord.RichEmbed()
+  .setAuthor(Kicked !, user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("User:",  '[ ' + ${user.tag} + ' ]')
+  .addField("By:", '[ ' + ${message.author.tag} + ' ]')
+  .addField("Reason:", '[ ' + ${reason} + ' ]')
+  client.channels.get("آي دي روم اللوق").send({embed : banembed})
+}
+});
 
 
 
