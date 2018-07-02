b993148ced0e33e4c7f08684cd268f8009051811
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-02 02:02:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -828,7 +828,7 @@ const Za7f = [
 });
 
 
-  var prefix = "+";
+
 var rebel = ["https://f.top4top.net/p_682it2tg6.png","https://e.top4top.net/p_682a1cus5.png","https://d.top4top.net/p_682pycol4.png","https://c.top4top.net/p_682vqehy3.png","https://b.top4top.net/p_682mlf9d2.png","https://a.top4top.net/p_6827dule1.png","https://b.top4top.net/p_682g1meb10.png","https://a.top4top.net/p_682jgp4v9.png","https://f.top4top.net/p_682d4joq8.png","https://e.top4top.net/p_6828o0e47.png","https://d.top4top.net/p_6824x7sy6.png","https://c.top4top.net/p_682gzo2l5.png","https://b.top4top.net/p_68295qg04.png","https://a.top4top.net/p_682zrz6h3.png","https://f.top4top.net/p_6828vkzc2.png","https://e.top4top.net/p_682i8tb11.png"]
     client.on('message', message => {
         var args = message.content.split(" ").slice(1);
@@ -1000,7 +1000,6 @@ const zead = [
 
 
 client.on('message', function(message) {
-  var prefix = '+';
     if(message.content.startsWith(prefix + 'قرعه')) {
         let args = message.content.split(" ").slice(1);
         if (!args[0]) {
@@ -1043,7 +1042,6 @@ const kingmas = [
    '*** روح اكل ملح + ليمون اذا مافيه اكل اي شيء من اختيار الي معك.  ***'
 ]
  client.on('message', message => {
-   var prefix = '+';
  if (message.content.startsWith(prefix + 'حكم')) {
   var mariam= new Discord.RichEmbed()
   .setTitle("لعبة حكم ..")
