cc5201cabab3a5c37035d3abf404b509ff22ab21
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-05 01:47:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -444,38 +444,7 @@ const prefix = "+";
 }
 });
 
-client.on('message', message => {
-    var prefix = "+"
-     let command = message.content.split(" ")[0];
-   command = command.slice(prefix.length);
- 
-   let args = message.content.split(" ").slice(1);
- 
- 
- if(command == "draw") {
-     var Canvas = require('canvas')
-   , Image = new Canvas.Image
-   , canvas = new Canvas(450, 170)
-     fs.readFile("./1.png", function (err, Background) {
-   , ctx = canvas.getContext('2d');
-   ctx.font = '30px Impact';
-   let args = message.content.split(" ").slice(1);
-   
- Image.src = canvas.toBuffer();
- 
-     console.log(Image);
- ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
- ctx.fillText(args.join("  "),110, 70);
- 
- 
- ctx.beginPath();
- ctx.lineTo(50, 102);
- ctx.stroke();
- 
- message.channel.sendFile(canvas.toBuffer());
- }
- 
- });
+
 
 
 client.on('message', message => {
