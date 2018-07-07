0709133dc7d5da7f540ed8f6f7e0176cf79a7575
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-07 02:40:51 -0230

    Update bot.js

--- bot.js
+++ bot.js
@@ -904,29 +904,7 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 
 });
 
-client.on('message', message => {
-    if (message.content.startsWith("+avatar")) {
-if(!message.channel.guild) return;
 
-        var mentionned = message.mentions.users.first();
-
-    var client;
-      if(mentionned){
-          var client = mentionned;
-      } else {
-          var client = message.author;
-
-      }
-
-        const embed = new Discord.RichEmbed()
-                           .addField('Requested by:', "<@" + message.author.id + ">")
-
-        .setColor(000000)
-        .setFooter(اسم بوتك, 'ur bot image link')
-        .setImage(${client.avatarURL})
-      message.channel.sendEmbed(embed);
-    }
-});
 	
 	client.on('message', message => {
     if (message.author.id === client.user.id) return;
