2a3bf0ee7dfe2dc391dbd3509fc1b821820ea7ed
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-25 17:11:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -666,7 +666,7 @@ client.on('guildCreate', guild => {
 
 
 
-const Discord = require("discord.js");
+
 const ytdl = require("ytdl-core");
 const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
@@ -674,7 +674,7 @@ const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
-const client = new Discord.Client();
+
 
 /*
 البكجآت
