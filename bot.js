883697af1464279cad0d7678da38346cea80f35b
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-25 17:13:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -693,7 +693,6 @@ client.on('ready', () => {
     client.user.setStatus("idle")
 });
 //by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
-const prefix = "+"
 client.on('message', async msg => { // eslint-disable-line
 	if (msg.author.bot) return undefined;
 	//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
